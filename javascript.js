92c41f311d787fbcf864435f7180911529290a57
BradieF <bradiefuentes2002@gmail.com>
2024-12-16 14:49:53 -0500

    Cleaned up spacing

--- javascript.js
+++ javascript.js
@@ -1,59 +1,55 @@
-
 let humanScore = 0;
 let computerScore = 0; 
 let rounds = 1;
-
+    
 
 const header = createElement("h1")
-updateText(header, "Select a button below to begin playing Rock  Paper Scissors!" )
-header.className = "header"
-document.body.appendChild(header);
+  updateText(header, "Select a button below to begin playing Rock  Paper Scissors!" )
+  header.className = "header"
+  document.body.appendChild(header);
 
 const buttonDiv = createElement('div')
-document.body.appendChild(buttonDiv);
-buttonDiv.id = "button-container";
+  document.body.appendChild(buttonDiv);
+  buttonDiv.id = "button-container";
 
 const scissorButton = createElement("button");
-scissorButton.className = "buttons"
-updateText(scissorButton, 'Scissors')
-buttonDiv.appendChild(scissorButton);
+  scissorButton.className = "buttons"
+  updateText(scissorButton, 'Scissors')
+  buttonDiv.appendChild(scissorButton);
 
 const paperButton = createElement("button");
-paperButton.className = "buttons"
-updateText(paperButton, 'Paper')
-buttonDiv.appendChild(paperButton);
+  paperButton.className = "buttons"
+  updateText(paperButton, 'Paper')
+  buttonDiv.appendChild(paperButton);
 
 const rockButton = createElement("button");
-rockButton.className = "buttons"
-updateText(rockButton, 'Rock')
-buttonDiv.appendChild(rockButton);
+  rockButton.className = "buttons"
+  updateText(rockButton, 'Rock')
+  buttonDiv.appendChild(rockButton);
+
+const scoreDiv = createElement('div')
+  buttonDiv.appendChild(scoreDiv);
+  scoreDiv.className = "scoreDiv"
+
+const resultDiv = createElement("div");
+  resultDiv.className = "resultDiv"
+  document.body.appendChild(resultDiv);
+
+
+const compScore = createElement('h3');
+  scoreDiv.appendChild(compScore)
+  updateText(compScore, "Computer Score: " + computerScore)
+  compScore.className = "compScore scores";
+
+
+const humScore = createElement('h3');
+  scoreDiv.appendChild(humScore)
+  humScore.textContent = "Your Score: " + humanScore;
+  humScore.className = "humScore scores";
 
 scissorButton.addEventListener("click", () => onClick("SCISSORS"));
 paperButton.addEventListener("click", () =>onClick("PAPER"));
 rockButton.addEventListener("click", () =>onClick("ROCK"));
-
-
-const scoreDiv = createElement('div')
-buttonDiv.appendChild(scoreDiv);
-scoreDiv.className = "scoreDiv"
-
-const resultDiv = createElement("div");
-resultDiv.className = "resultDiv"
-document.body.appendChild(resultDiv);
-
-
-const compScore = createElement('h3');
-scoreDiv.appendChild(compScore)
-updateText(compScore, "Computer Score: " + computerScore)
-compScore.className = "compScore scores";
-
-
-const humScore = createElement('h3');
-scoreDiv.appendChild(humScore)
-humScore.textContent = "Your Score: " + humanScore;
-humScore.className = "humScore scores";
-
-
 
 
 function onClick(selection){
@@ -74,114 +70,102 @@ function onClick(selection){
 }
 
 function updateText(element, text){
-    return element.textContent = text;
+  return element.textContent = text;
 }
-
 
 function createElement(element){
-   return document.createElement(element)
+  return document.createElement(element)
 }
 
-
-
 function getComputerChoice() {
-    let choice = Math.random() * 100;
+  let choice = Math.random() * 100;
 
-    if (choice < 33) return "SCISSORS";
-     else if (choice < 66) return "ROCK";
-     else return "PAPER";
+  if (choice < 33) return "SCISSORS";
+    else if (choice < 66) return "ROCK";
+    else return "PAPER";
  }
 
-
- function calcScore (winner){
-    if (winner == "computer" ){
-        computerScore++;
-        updateText(compScore, "Computer Score: " + computerScore)
+function calcScore (winner){
+  if (winner == "computer" ){
+    computerScore++;
+    updateText(compScore, "Computer Score: " + computerScore)
     }
-    else if (winner == "human"){
-        humanScore++;
-        updateText(humScore, "Your Score: " + humanScore)
+  else if (winner == "human"){
+    humanScore++;
+    updateText(humScore, "Your Score: " + humanScore)
     } 
  }
 
-
- function gameOver (humanScore, computerScore){
-     if (humanScore == 5 || computerScore == 5){
-        const gameWinner = createElement("h2")
-        if (humanScore == 5){
-            updateText(gameWinner, "Game Over: You Win!")
-            resultDiv.appendChild(gameWinner);
-        }
-        else if (computerScore == 5) {
-            updateText(gameWinner, "Game Over: Computer Wins!")
-            resultDiv.appendChild(gameWinner);
-
-        }
-        else if (computerScore == 5 && humanScore == 5) {
-            updateText(gameWinner, "Game Over: It's a TIE!")
-            resultDiv.appendChild(gameWinner);
-
-        } 
-        
+function gameOver (humanScore, computerScore){
+  if (humanScore == 5 || computerScore == 5){
+    const gameWinner = createElement("h2")
+  if (humanScore == 5){
+    updateText(gameWinner, "Game Over: You Win!")
+    resultDiv.appendChild(gameWinner);
+  }
+  else if (computerScore == 5) {
+    updateText(gameWinner, "Game Over: Computer Wins!")
+    resultDiv.appendChild(gameWinner);
+  }
+  else if (computerScore == 5 && humanScore == 5) {
+    updateText(gameWinner, "Game Over: It's a TIE!")
+    resultDiv.appendChild(gameWinner);
+  } 
     }
  }
 
- function restartRound (){
-        updateText(resultDiv, '')
-        updateText(compScore, 'Computer Score: 0')
-        updateText(humScore, 'Human Score: 0')
-        humanScore = 0;
-        computerScore = 0;
-        rounds = 1;
-    
+function restartRound (){
+  updateText(resultDiv, '')
+  updateText(compScore, 'Computer Score: 0')
+  updateText(humScore, 'Human Score: 0')
+  humanScore = 0;
+  computerScore = 0;
+  rounds = 1;
  }
-
 
 function playRound(computerChoice, humanChoice)
  {
     const comInput = createElement("p")
-    updateText(comInput, "Computer chose: " + computerChoice)
-    resultDiv.appendChild(comInput);
+      updateText(comInput, "Computer chose: " + computerChoice)
+      resultDiv.appendChild(comInput);
 
     const humInput = createElement("p")
-    updateText(humInput, "You chose: " + humanChoice)
-    resultDiv.appendChild(humInput);
+      updateText(humInput, "You chose: " + humanChoice)
+      resultDiv.appendChild(humInput);
 
     if (humanChoice == computerChoice){
-        const tie = createElement("p")
+      const tie = createElement("p")
         tie.className = "whoWon tie"
         updateText(tie, 'Its a TIE!')
         resultDiv.appendChild(tie);
 
-        return "tie";
+      return "tie";
     }
     else if ((humanChoice == "ROCK" && computerChoice == "SCISSORS") 
               || (humanChoice == "SCISSORS" && computerChoice == "PAPER")
-              || (humanChoice == "PAPER" && computerChoice == "ROCK"))
-        {
-            const humanWon = createElement("p")
-            humanWon.className = "whoWon humanWon"
-            updateText(humanWon, 'You win!')
-            resultDiv.appendChild(humanWon);
+              || (humanChoice == "PAPER" && computerChoice == "ROCK")) {
+        const humanWon = createElement("p")
+          humanWon.className = "whoWon humanWon"
+          updateText(humanWon, 'You win!')
+          resultDiv.appendChild(humanWon);
     
-            return "human";
+        return "human";
         }
         else if ((computerChoice == "ROCK" && humanChoice == "SCISSORS") 
             || (computerChoice == "SCISSORS" && humanChoice == "PAPER")
             || (computerChoice == "PAPER" && humanChoice == "ROCK"))
             {
-                const computerWon = createElement("p")
-                computerWon.className = "whoWon computerWon"
-                updateText(computerWon, 'Computer wins!')
-                resultDiv.appendChild(computerWon);
+            const computerWon = createElement("p")
+              computerWon.className = "whoWon computerWon"
+              updateText(computerWon, 'Computer wins!')
+              resultDiv.appendChild(computerWon);
 
-                return "computer";
+            return "computer";
             }
-    else 
-     {
+    else {
         const error = createElement("p")
-        updateText(error, "Error, incorrect option selected")
-        resultDiv.appendChild(error);       
+          updateText(error, "Error, incorrect option selected")
+          resultDiv.appendChild(error);       
      }
 }
 
